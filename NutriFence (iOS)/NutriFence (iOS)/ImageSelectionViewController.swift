e3a5df6181c773669c41986f311a48a97abc739a
mgwatt0521 <mgwatt0521@gmail.com>
2016-11-05 11:46:34 -0500

    Fix TGCamera link :)

--- NutriFence (iOS)/NutriFence (iOS)/ImageSelectionViewController.swift
+++ NutriFence (iOS)/NutriFence (iOS)/ImageSelectionViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class ImageSelectionViewController: UIViewController {
+class ImageSelectionViewController: UIViewController, TGCameraDelegate {
 
+    var googleAPIKey = "AIzaSyDbLtbxBhXGUmQpRyeKQPryCSZZjeKoKmc"
+    var googleURL: URL {
+        return URL(string: "https://vision.googleapis.com/v1/images:annotate?key=\(googleAPIKey)")!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,4 +38,145 @@ class ImageSelectionViewController: UIViewController {
     }
     */
 
+}
+
+// Image Processing
+extension ImageSelectionViewController {
+    func analyzeResults(_ dataToParse: Data) {
+        
+        // Update UI on the main thread
+        DispatchQueue.main.async(execute: {
+            
+            
+            // Use SwiftyJSON to parse results
+            let json = JSON(data: dataToParse)
+            let errorObj: JSON = json["error"]
+            
+            
+            // Check for errors
+            if (errorObj.dictionaryValue != [:]) {
+                // self.labelResults.text = "Error code \(errorObj["code"]): \(errorObj["message"])"
+            } else {
+                // Parse the response
+                print(json)
+                /*let responses: JSON = json["responses"][0]
+                 
+                 // Get label annotations
+                 let labelAnnotations: JSON = responses["labelAnnotations"]
+                 let numLabels: Int = labelAnnotations.count
+                 var labels: Array<String> = []
+                 if numLabels > 0 {
+                 var labelResultsText:String = "Labels found: "
+                 for index in 0..<numLabels {
+                 let label = labelAnnotations[index]["description"].stringValue
+                 labels.append(label)
+                 }
+                 for label in labels {
+                 // if it's not the last item add a comma
+                 if labels[labels.count - 1] != label {
+                 labelResultsText += "\(label), "
+                 } else {
+                 labelResultsText += "\(label)"
+                 }
+                 }
+                 self.labelResults.text = labelResultsText
+                 } else {
+                 self.labelResults.text = "No labels found"
+                 }
+                 */
+            }
+        })
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.isHidden = false
+            selectedImage = pickedImage
+            labelResults.isHidden = true
+            
+            // Base64 encode the image and create the request
+            let binaryImageData = base64EncodeImage(pickedImage)
+            createRequest(with: binaryImageData)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func resizeImage(_ imageSize: CGSize, image: UIImage) -> Data {
+        UIGraphicsBeginImageContext(imageSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        let resizedImage = UIImagePNGRepresentation(newImage!)
+        UIGraphicsEndImageContext()
+        return resizedImage!
+    }
+}
+
+
+// Networking
+
+extension ImageSelectionViewController {
+    func base64EncodeImage(_ image: UIImage) -> String {
+        var imagedata = UIImagePNGRepresentation(image)
+        
+        // Resize the image if it exceeds the 2MB API limit
+        if ((imagedata?.count)! > 2097152) {
+            let oldSize: CGSize = image.size
+            let newSize: CGSize = CGSize(width: 800, height: oldSize.height / oldSize.width * 800)
+            imagedata = resizeImage(newSize, image: image)
+        }
+        
+        return imagedata!.base64EncodedString(options: .endLineWithCarriageReturn)
+    }
+    
+    func createRequest(with imageBase64: String) {
+        // Create our request URL
+        
+        var request = URLRequest(url: googleURL)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(Bundle.main.bundleIdentifier ?? "", forHTTPHeaderField: "X-Ios-Bundle-Identifier")
+        
+        // Build our API request
+        let jsonRequest = [
+            "requests": [
+                "image": [
+                    "content": imageBase64
+                ],
+                "features": [
+                    [
+                        "type": "TEXT_DETECTION",
+                        "maxResults": 10
+                    ]
+                ]
+            ]
+        ]
+        let jsonObject = JSON(jsonDictionary: jsonRequest)
+        
+        // Serialize the JSON
+        guard let data = try? jsonObject.rawData() else {
+            return
+        }
+        
+        request.httpBody = data
+        
+        // Run the request on a background thread
+        DispatchQueue.global().async { self.runRequestOnBackgroundThread(request) }
+    }
+    
+    func runRequestOnBackgroundThread(_ request: URLRequest) {
+        // run the request
+        
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            
+            self.analyzeResults(data)
+        }
+        
+        task.resume()
+    }
 }

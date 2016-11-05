6a10a39a4f48ac0e41fd3d0be7a3994560cf8b01
Isabelle Sauve <isabibble@gmail.com>
2016-11-05 06:46:23 -0400

    "Fixed" the lowercase issue

--- Classification API/app.js
+++ Classification API/app.js
@@ -10,11 +10,13 @@ var populateDatabase = require("./populateDatabase.js");
 var unsafeList;
 client.smembers('Celiac Unsafe', function(err, list) {
     unsafeList = list;
+    unsafeList.forEach(function(item) { item.toLowerCase() });
 })
 
 var unfriendlyList;
 client.smembers('Celiac Unfriendly', function(err, list) {
     unfriendlyList = list;
+    unfriendlyList.forEach(function(item) { item.toLowerCase() });
 })
 
 //require the express nodejs module

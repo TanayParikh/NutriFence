a3abfb9312070f1691326ca9d5fcea411cab4c4e
mgwatt0521 <mgwatt0521@gmail.com>
2016-11-04 20:32:47 -0500

    Xcode project files

---
+++ NutriFence (iOS)/NutriFence (iOS)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NutriFence (iOS)
+//
+//  Created by Matthew Watt on 11/4/16.
+//  Copyright © 2016 NutriFence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

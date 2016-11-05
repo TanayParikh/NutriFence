73c4eeb033a78fe9a45a68b9286d068e099eecd1
mgwatt0521 <mgwatt0521@gmail.com>
2016-11-05 10:46:46 -0500

    Added source for custom camera

--- NutriFence (iOS)/NutriFence (iOS)/OpeningSplashViewController.swift
+++ NutriFence (iOS)/NutriFence (iOS)/OpeningSplashViewController.swift
@@ -8,27 +8,66 @@
 
 import UIKit
 
-class OpeningSplashViewController: UIViewController {
+@IBDesignable
+class OpeningSplashViewController: UIViewController, UITableViewDataSource {
     
-    var backgroundGradient: CAGradientLayer!
+    private var backgroundGradient: CAGradientLayer!
+    @IBInspectable var gradientTopColor: UIColor!
+    @IBInspectable var gradientBottomColor: UIColor!
+    @IBOutlet weak var selectionTableView: UITableView!
+    static let rgbGrayFontColor = 234
+    let selections = [
+        "Lactose Intolerant",
+        "Gluten Free",
+        "Vegan",
+        "Peanut Allergic"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        selectionTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        createGradientLayer()
+        createGradientLayer(top: gradientTopColor, bottom: gradientBottomColor)
     }
     
-    func createGradientLayer() {
+    // Data source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return selections.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let selection = selections[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SelectionCell")
+        cell?.textLabel?.font = UIFont(name: "Century Gothic", size: 17)
+        cell?.textLabel?.textColor = OpeningSplashViewController.customGray()
+        cell?.textLabel?.text = selection
+        cell?.indentationLevel = 0
+        cell?.preservesSuperviewLayoutMargins = false
+        cell?.layoutMargins = UIEdgeInsets.zero
+        return cell!
+    }
+    
+    // Custom drawing
+    
+    func createGradientLayer(top: UIColor, bottom: UIColor) {
         backgroundGradient = CAGradientLayer()
         backgroundGradient.frame = self.view.bounds
-        let topColor = UIColor(red: 69, green: 58, blue: 73).cgColor
-        let bottomColor = UIColor(red: 62, green: 58, blue: 73).cgColor
-        backgroundGradient.colors = [topColor, bottomColor]
+        backgroundGradient.colors = [top.cgColor, bottom.cgColor]
         self.view.layer.insertSublayer(backgroundGradient, at: 0)
     }
     
+    // Helpers
+    
+    static func customGray() -> UIColor {
+        return UIColor(red: rgbGrayFontColor, green: rgbGrayFontColor, blue: rgbGrayFontColor)
+    }
 }

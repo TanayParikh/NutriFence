a8fcdd7f2353f5e55721e63aed1e5140ecceda00
mgwatt0521 <mgwatt0521@gmail.com>
2016-11-05 11:44:20 -0500

    Link in TGCamera library

---
+++ NutriFence (iOS)/NutriFence (iOS)/ImageSelectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImageSelectionViewController.swift
+//  NutriFence (iOS)
+//
+//  Created by Matthew Watt on 11/5/16.
+//  Copyright © 2016 NutriFence. All rights reserved.
+//
+
+import UIKit
+
+class ImageSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

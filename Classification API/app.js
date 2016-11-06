82783cd49d6663f324df8c09b102a49074f61595
Isabelle Sauve <isabibble@gmail.com>
2016-11-06 10:44:32 -0500

    Autocorrect fixed

--- Classification API/app.js
+++ Classification API/app.js
@@ -49,17 +49,110 @@ app.listen(3000, function () {
     console.log('Server is running. Point your browser to: http://localhost:3000');
 });
 
-function interpretAndSendData(req, res) {
+var res;
+function interpretAndSendData(req, response) {
+    res = response;
     var rawIngredients = req.body.responses[0].textAnnotations[0].description;
-    rawIngredients = formatRawIngredients(rawIngredients);
     // rawIngredients = splitIntoSubgroups(rawIngredients);             // adds supheadings - buggy though
+
+    // format the raw ingredients
+    rawIngredients = rawIngredients.toLowerCase();
+    var index = rawIngredients.indexOf("ingredients") + 13;
+    rawIngredients = rawIngredients.substring(index, rawIngredients.length - 1);
+    rawIngredients = rawIngredients.replace(/(\n)+/g, ' ');
+    rawIngredients = rawIngredients.replace(/[.]+/g, ',');
+
+    console.log("\nbefore: " + rawIngredients);
+    spellCheckIngredients(rawIngredients);
+
+    // unused function for adding subheadings
+    function splitIntoSubgroups(rawIngredients) {
+        var subGroupHead = null;
+
+        for (var i = 0; i < rawIngredients.length - 1; ++i) {
+            var ingredient = rawIngredients[i];
+
+            if (ingredient.indexOf('(') != -1) {
+                var index = ingredient.indexOf('(');
+                subGroupHead = ingredient.substr(0, index - 1); //ingredient.replace(/[(]+/g, '');
+                rawIngredients[i] = ingredient.substr(index + 1) + "(" + subGroupHead + ")";
+            } else if ((ingredient.indexOf(')') != -1) && subGroupHead) {
+                rawIngredients[i] = ingredient.replace(/[)]+/g, '') + " (" + subGroupHead + ")";
+
+                subGroupHead = null;
+            } else if (subGroupHead) {
+                rawIngredients[i] = ingredient + " (" + subGroupHead + ")";
+            }
+        }
+        return rawIngredients;
+    }
+}
+
+function spellCheckIngredients(rawIngredients) {
+    var request = require('request');
+
+    // Set the headers
+    var headers = {
+        'Ocp-Apim-Subscription-Key': autoCorrectAPIKey
+        //'Content-Type':     'application/x-www-form-urlencoded'
+    };
+
+    // Configure the request
+    var options = {
+        url: 'https://api.cognitive.microsoft.com/bing/v5.0/spellcheck/',
+        method: 'GET',
+        headers: headers,
+        qs: {'text': rawIngredients, 'mode': 'proof'}
+    };
+
+    // Start the request
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            rawIngredients = correctSpellingErrors(body, rawIngredients);
+        }
+
+        console.log("ERROR:  " + error);
+        console.log("STATUS:  " + response.statusCode);
+        //console.log(body);
+
+        formatRawIngredients2(rawIngredients);
+    });
+
+    function correctSpellingErrors(response, rawIngredients) {
+        var spellChecked = JSON.parse(response);
+
+        /*for (var i = 0; i < spellChecked.flaggedTokens.length; ++i) {
+         var word = spellChecked.flaggedTokens[i];
+         rawIngredients = rawIngredients.replace(word.token, word.suggestions[0].suggestion);
+         }*/
+
+        spellChecked.flaggedTokens.forEach(function (word) {
+            rawIngredients = rawIngredients.replace(word.token, word.suggestions[0].suggestion);
+            //console.log(word.token + " to be replaced with ");
+            //console.log(word.suggestions[0].suggestion);
+        });
+
+        return rawIngredients;
+    }
+}
+
+function formatRawIngredients2(rawIngredients) {
+    console.log("\nafter: " + rawIngredients);
+
+    rawIngredients = rawIngredients.replace(/[)]+/g, ',');
+    rawIngredients = rawIngredients.replace(/[(]+/g, '(,');
+    rawIngredients = rawIngredients.split(",");
+
+    for (var i=0; i<rawIngredients.length; i++) {
+        if (rawIngredients[i].includes("(")) rawIngredients.splice(i, 1);
+        rawIngredients[i] = rawIngredients[i].replace(/^\s+|\s+$/g, '');
+    }
 
     var mayContain = [];
     var goodIngredients = [];
     var badIngredients = [];
     var unsureIngredients = [];
     var pass, mayContainGluten;
-
 
     for (var k=0; k<rawIngredients.length; k++) {
         parseMayContain(rawIngredients[k], k);
@@ -84,82 +177,6 @@ function interpretAndSendData(req, res) {
         mayContainGluten: mayContainGluten
     }));
 
-    /********************* Helper functions **************************/
-    function formatRawIngredients(rawIngredients) {
-        rawIngredients = rawIngredients.toLowerCase();
-        var index = rawIngredients.indexOf("ingredients") + 13;
-        rawIngredients = rawIngredients.substring(index, rawIngredients.length - 1);
-        rawIngredients = rawIngredients.replace(/(\n)+/g, ' ');
-        rawIngredients = rawIngredients.replace(/[.]+/g, ',');
-
-        spellCheckIngredients(rawIngredients);
-
-        console.log(rawIngredients);
-
-        rawIngredients = rawIngredients.replace(/[)]+/g, ',');
-        rawIngredients = rawIngredients.replace(/[(]+/g, '(,');
-        rawIngredients = rawIngredients.split(",");
-
-        for (var i=0; i<rawIngredients.length; i++) {
-            if (rawIngredients[i].includes("(")) rawIngredients.splice(i, 1);
-            rawIngredients[i] = rawIngredients[i].replace(/^\s+|\s+$/g, '');
-        }
-
-        return rawIngredients;
-    }
-
-    function spellCheckIngredients(rawIngredients) {
-        var request = require('request');
-
-        // Set the headers
-        var headers = {
-            'Ocp-Apim-Subscription-Key': autoCorrectAPIKey
-            //'Content-Type':     'application/x-www-form-urlencoded'
-        };
-
-        // Configure the request
-        var options = {
-            url: 'https://api.cognitive.microsoft.com/bing/v5.0/spellcheck/',
-            method: 'GET',
-            headers: headers,
-            qs: {'text': rawIngredients, 'mode': 'proof'}
-        };
-
-        // Start the request
-        request(options, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                rawIngredients = correctSpellingErrors(body, rawIngredients);
-                console.log("3:" + rawIngredients);
-
-            }
-
-            console.log("ERROR:  " + error);
-
-            //console.log("STATUS:  " + response.statusCode);
-            //console.log(body);
-        });
-
-        console.log("2:" + rawIngredients);
-
-        return rawIngredients;
-    }
-
-    function correctSpellingErrors(response, rawIngredients) {
-        var spellChecked = JSON.parse(response);
-
-        /*for (var i = 0; i < spellChecked.flaggedTokens.length; ++i) {
-            var word = spellChecked.flaggedTokens[i];
-            rawIngredients = rawIngredients.replace(word.token, word.suggestions[0].suggestion);
-        }*/
-
-        spellChecked.flaggedTokens.forEach(function (word) {
-            rawIngredients = rawIngredients.replace(word.token, word.suggestions[0].suggestion);
-            //console.log(word.token + " to be replaced with ");
-            //console.log(word.suggestions[0].suggestion);
-        });
-
-        return rawIngredients;
-    }
 
     function parseMayContain(ingredient, index) {
         if (ingredient.includes("may contain")) {
@@ -198,27 +215,5 @@ function interpretAndSendData(req, res) {
         });
 
         return unfriendly;
-    }
-
-    // unused function for adding subheadings
-    function splitIntoSubgroups(rawIngredients) {
-        var subGroupHead = null;
-
-        for (var i = 0; i < rawIngredients.length - 1; ++i) {
-            var ingredient = rawIngredients[i];
-
-            if (ingredient.indexOf('(') != -1) {
-                var index = ingredient.indexOf('(');
-                subGroupHead = ingredient.substr(0, index - 1); //ingredient.replace(/[(]+/g, '');
-                rawIngredients[i] = ingredient.substr(index + 1) + "(" + subGroupHead + ")";
-            } else if ((ingredient.indexOf(')') != -1) && subGroupHead) {
-                rawIngredients[i] = ingredient.replace(/[)]+/g, '') + " (" + subGroupHead + ")";
-
-                subGroupHead = null;
-            } else if (subGroupHead) {
-                rawIngredients[i] = ingredient + " (" + subGroupHead + ")";
-            }
-        }
-        return rawIngredients;
     }
 }

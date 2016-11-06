1b3c1a4019a243a5bdc0498294f397d5bbb09853
Isabelle Sauve <isabibble@gmail.com>
2016-11-05 20:38:14 -0400

    Refactored app.js

--- Classification API/app.js
+++ Classification API/app.js
@@ -1,7 +1,6 @@
 // start Redis
 var redis = require('redis');
 var client = redis.createClient();
-
 client.on('connect', function() {
     console.log('app connected');
 });
@@ -10,13 +9,12 @@ client.on('connect', function() {
 var populateDatabase = require("./populateDatabase.js");
 populateDatabase.addCeliacUnsafe();
 populateDatabase.addCeliacUnfriendly();
-// populateDatabase.printMembers();
+// populateDatabase.printMembers();                         // uncomment for testing
 
 var unsafeList;
 client.smembers('Celiac Unsafe', function(err, list) {
     unsafeList = list;
 });
-
 var unfriendlyList;
 client.smembers('Celiac Unfriendly', function(err, list) {
     unfriendlyList = list;
@@ -36,60 +34,95 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'www')));
 
 // tell express what to do when the /ClassificationAPI route is requested
-app.post('/ClassificationAPI',function(req, res){
+app.post('/ClassificationAPI', function(request, response) {
+    interpretAndSendData(request, response);
+});
+
+// wait for a connection
+app.listen(3000, function () {
+    console.log('Server is running. Point your browser to: http://localhost:3000');
+});
+
+function interpretAndSendData(req, res) {
     var rawIngredients = req.body.responses[0].textAnnotations[0].description;
-    rawIngredients = rawIngredients.toLowerCase();
-
-    var index = rawIngredients.indexOf("ingredients") + 13;
-    rawIngredients = rawIngredients.substring(index, rawIngredients.length-1);
-    rawIngredients = rawIngredients.replace(/(\n)+/g, ' ');
-    rawIngredients = rawIngredients.replace(/[.]+/g, ',');
-    rawIngredients = rawIngredients.split(",");
-
-    // Parses ingredients with sub-group
-    var subGroupHead = null;
-
-    for (var i = 0; i < rawIngredients.length-1; ++i) {
-        var ingredient = rawIngredients[i];
-
-        if (ingredient.indexOf('(') != -1) {
-            var index = ingredient.indexOf('(');
-            subGroupHead = ingredient.substr(0, index-1); //ingredient.replace(/[(]+/g, '');
-            rawIngredients[i] = ingredient.substr(index+1) + "(" + subGroupHead + ")";
-        } else if ((ingredient.indexOf(')') != -1) && subGroupHead) {
-            rawIngredients[i] = ingredient.replace(/[)]+/g, '') + " (" + subGroupHead + ")";
-            
-            subGroupHead = null;
-        } else if (subGroupHead) {
-            rawIngredients[i] = ingredient + " (" + subGroupHead + ")";
-        }
-    }
+    rawIngredients = formatRawIngredients(rawIngredients);
+    rawIngredients = splitIntoSubgroups(rawIngredients);
 
     var mayContain = [];
     var goodIngredients = [];
     var badIngredients = [];
     var unsureIngredients = [];
+    var pass, mayContainGluten;
 
     for (var k=0; k<rawIngredients.length; k++) {
-        if ( rawIngredients[k].includes("may contain") ) {
-            var temp = rawIngredients[k].substring(rawIngredients[k].indexOf("may contain")+12, rawIngredients[k].length);
-            mayContain.push(temp);
-            for (var j=k+1; j<rawIngredients.length; j++) {
-                mayContain.push(rawIngredients[j]);
-            }
-
-            rawIngredients.splice(k, rawIngredients.length-k);
+        if (rawIngredients[k].includes("may contain")) {
+            parseMayContain(rawIngredients[k], k);
+            rawIngredients.splice(k, rawIngredients.length - k);
+            mayContainGluten = true;
         }
     }
 
-    rawIngredients.forEach( function(ingredient) {
+    rawIngredients.forEach(function (ingredient) {
         if (!isUnsafe(ingredient) && !isUnfriendly(ingredient)) {
             if (ingredient) goodIngredients.push(ingredient);
         }
     });
 
+    pass = !(badIngredients.length != 0);
+    if (!mayContainGluten) mayContainGluten = (unsureIngredients.length != 0);
+
+    res.setHeader('Content-Type', 'application/rawIngredients');
+    res.send(JSON.stringify({
+        Bad_Ingredients: badIngredients,
+        Unsure_Ingredients: unsureIngredients,
+        Good_Ingredients: goodIngredients,
+        May_Contain: mayContain,
+        isGlutenFree: pass,
+        mayContainGluten: mayContainGluten
+    }));
+
+    /********************* Helper functions **************************/
+    function formatRawIngredients(rawIngredients) {
+        rawIngredients = rawIngredients.toLowerCase();
+        var index = rawIngredients.indexOf("ingredients") + 13;
+        rawIngredients = rawIngredients.substring(index, rawIngredients.length - 1);
+        rawIngredients = rawIngredients.replace(/(\n)+/g, ' ');
+        rawIngredients = rawIngredients.replace(/[.]+/g, ',');
+        rawIngredients = rawIngredients.split(",");
+        return rawIngredients;
+    }
+
+    function splitIntoSubgroups(rawIngredients) {
+        var subGroupHead = null;
+
+        for (var i = 0; i < rawIngredients.length - 1; ++i) {
+            var ingredient = rawIngredients[i];
+
+            if (ingredient.indexOf('(') != -1) {
+                var index = ingredient.indexOf('(');
+                subGroupHead = ingredient.substr(0, index - 1); //ingredient.replace(/[(]+/g, '');
+                rawIngredients[i] = ingredient.substr(index + 1) + "(" + subGroupHead + ")";
+            } else if ((ingredient.indexOf(')') != -1) && subGroupHead) {
+                rawIngredients[i] = ingredient.replace(/[)]+/g, '') + " (" + subGroupHead + ")";
+
+                subGroupHead = null;
+            } else if (subGroupHead) {
+                rawIngredients[i] = ingredient + " (" + subGroupHead + ")";
+            }
+        }
+        return rawIngredients;
+    }
+
+    function parseMayContain(ingredient, index) {
+        var temp = ingredient.substring(ingredient.indexOf("may contain") + 12, ingredient.length);
+        mayContain.push(temp);
+        for (var i = index + 1; i < rawIngredients.length; i++) {
+            mayContain.push(rawIngredients[i]);
+        }
+    }
+
     function isUnsafe(ingredient) {
-        unsafeList.forEach( function (unsafeItem) {
+        unsafeList.forEach(function (unsafeItem) {
             if (ingredient.includes(unsafeItem)) {
                 badIngredients.push(ingredient);
                 return true;
@@ -109,23 +142,4 @@ app.post('/ClassificationAPI',function(req, res){
 
         return false;
     }
-    
-    var pass = !(badIngredients.length != 0);
-    var maybe = (unsureIngredients.length != 0);
-
-    res.setHeader('Content-Type', 'application/rawIngredients');
-
-    res.send(JSON.stringify({
-        Bad_Ingredients: badIngredients,
-        Unsure_Ingredients: unsureIngredients,
-        Good_Ingredients: goodIngredients,
-        May_Contain: mayContain,
-        isGlutenFree: pass,
-        mayContainGluten: maybe
-    }));
-});
-
-//wait for a connection
-app.listen(3000, function () {
-    console.log('Server is running. Point your browser to: http://localhost:3000');
-});
+}

0329979f90ef78f4557d0f467fd28087050188a9
Isabelle Sauve <isabibble@gmail.com>
2016-11-05 21:10:54 -0400

    App.js with no bugs (but no subgroups)

---
+++ Classification API/appV2.js
@@ -0,0 +1,137 @@
+// start Redis
+var redis = require('redis');
+var client = redis.createClient();
+client.on('connect', function() {
+    console.log('app connected');
+});
+
+// fetch the unsafe/unfriendly ingredients data from Redis
+var populateDatabase = require("./populateDatabase.js");
+populateDatabase.addCeliacUnsafe();
+populateDatabase.addCeliacUnfriendly();
+// populateDatabase.printMembers();                         // uncomment for testing
+
+var unsafeList;
+client.smembers('Celiac Unsafe', function(err, list) {
+    unsafeList = list;
+});
+var unfriendlyList;
+client.smembers('Celiac Unfriendly', function(err, list) {
+    unfriendlyList = list;
+});
+
+// require and initialize the necessary modules
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    path = require("path");
+
+// add support for parsing different types of post data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// tell express that www is the root of our public web folder
+app.use(express.static(path.join(__dirname, 'www')));
+
+// tell express what to do when the /ClassificationAPI route is requested
+app.post('/ClassificationAPI', function(request, response) {
+    interpretAndSendData(request, response);
+});
+
+// wait for a connection
+app.listen(3000, function () {
+    console.log('Server is running. Point your browser to: http://localhost:3000');
+});
+
+function interpretAndSendData(req, res) {
+    var rawIngredients = req.body.responses[0].textAnnotations[0].description;
+    rawIngredients = formatRawIngredients(rawIngredients);
+
+    var mayContain = [];
+    var goodIngredients = [];
+    var badIngredients = [];
+    var unsureIngredients = [];
+    var pass, mayContainGluten;
+
+
+    for (var k=0; k<rawIngredients.length; k++) {
+        parseMayContain(rawIngredients[k], k);
+    }
+
+    rawIngredients.forEach(function (ingredient) {
+        if (!isUnsafe(ingredient) && !isUnfriendly(ingredient)) {
+            if (ingredient) goodIngredients.push(ingredient);
+        }
+    });
+
+    pass = !(badIngredients.length != 0);
+    if (!mayContainGluten) mayContainGluten = (unsureIngredients.length != 0);
+
+    res.setHeader('Content-Type', 'application/rawIngredients');
+    res.send(JSON.stringify({
+        Bad_Ingredients: badIngredients,
+        Unsure_Ingredients: unsureIngredients,
+        Good_Ingredients: goodIngredients,
+        May_Contain: mayContain,
+        isGlutenFree: pass,
+        mayContainGluten: mayContainGluten
+    }));
+
+    /********************* Helper functions **************************/
+    function formatRawIngredients(rawIngredients) {
+        rawIngredients = rawIngredients.toLowerCase();
+        var index = rawIngredients.indexOf("ingredients") + 13;
+        rawIngredients = rawIngredients.substring(index, rawIngredients.length - 1);
+        rawIngredients = rawIngredients.replace(/(\n)+/g, ' ');
+        rawIngredients = rawIngredients.replace(/[.]+/g, ',');
+        rawIngredients = rawIngredients.replace(/[)]+/g, ',');
+        rawIngredients = rawIngredients.replace(/[(]+/g, '(,');
+        rawIngredients = rawIngredients.split(",");
+
+        for (var i=0; i<rawIngredients.length; i++) {
+            if (rawIngredients[i].includes("(")) rawIngredients.splice(i, 1);
+            rawIngredients[i] = rawIngredients[i].replace(/^\s+|\s+$/g, '');
+        }
+
+        return rawIngredients;
+    }
+
+    function parseMayContain(ingredient, index) {
+        if (ingredient.includes("may contain")) {
+            var temp = ingredient.substring(ingredient.indexOf("may contain") + 13, ingredient.length);
+            if (temp.includes("wheat")) mayContainGluten = true;
+            mayContain.push(temp);
+
+            for (var i = index + 1; i < rawIngredients.length; i++) {
+                if (rawIngredients[i].includes("wheat")) mayContainGluten = true;
+                mayContain.push(rawIngredients[i]);
+            }
+
+            rawIngredients.splice(k, rawIngredients.length - k);
+        }
+    }
+
+    function isUnsafe(ingredient) {
+        var unsafe = false;
+        unsafeList.forEach(function (unsafeItem) {
+            if (ingredient.includes(unsafeItem)) {
+                badIngredients.push(ingredient);
+                unsafe = true;
+            }
+        });
+
+        return unsafe;
+    }
+
+    function isUnfriendly(ingredient) {
+        var unfriendly = false;
+        unfriendlyList.forEach(function (unfriendlyItem) {
+            if (ingredient.includes(unfriendlyItem)) {
+                unsureIngredients.push(ingredient);
+                unfriendly = true;
+            }
+        });
+
+        return unfriendly;
+    }
+}

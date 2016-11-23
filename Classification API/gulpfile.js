9dbd1fcad2492d45c3bfae739401af8b9597a794
Tanay <tanay@webexposure.ca>
2016-11-22 21:42:24 -0500

    Updated gulpfile for realtime typescript compilation

--- Classification API/gulpfile.js
+++ Classification API/gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
 var nodemon = require('gulp-nodemon');
 
-gulp.task('default', function() {
+gulp.task('typescript', function() {
+    console.log('Compiling typescript');
+    return gulp.src(['./*.ts'])
+        .pipe(ts({module: 'commonjs'})).js.pipe(gulp.dest('./deploy'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./*.ts', ['typescript']);
+});
+
+gulp.task('serve', ['typescript'], function () {
     nodemon({
-        script: 'app.js',
+        script: './deploy/app.js',
         ext: 'js',
         env: {
             PORT:3000
@@ -12,5 +23,12 @@ gulp.task('default', function() {
     })
     .on('restart', function() {
         console.log('Restarting');
-    })
+    });
 });
+
+gulp.task('deploy', ['serve'], function() {
+    return gulp.src(['package.json'])
+        .pipe(gulp.dest('./deploy'));
+});
+
+gulp.task('default', ['deploy']);

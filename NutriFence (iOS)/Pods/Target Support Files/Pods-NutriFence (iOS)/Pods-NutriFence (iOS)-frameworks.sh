2af3f3cff8088f59254a7d5f4687d7637154db5a
mgwatt0521 <mgwatt0521@gmail.com>
2016-11-23 19:03:30 -0600

    Added ALCameraView library

--- NutriFence (iOS)/Pods/Target Support Files/Pods-NutriFence (iOS)/Pods-NutriFence (iOS)-frameworks.sh
+++ NutriFence (iOS)/Pods/Target Support Files/Pods-NutriFence (iOS)/Pods-NutriFence (iOS)-frameworks.sh
@@ -84,10 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/ALCameraViewController/ALCameraViewController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/NVActivityIndicatorView/NVActivityIndicatorView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/ALCameraViewController/ALCameraViewController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/NVActivityIndicatorView/NVActivityIndicatorView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi

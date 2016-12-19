06db0e40e9f234259e3d4a3c6d7e7b30fe649794
mgwatt0521 <mgwatt0521@gmail.com>
2016-12-19 14:40:03 -0600

    Custom camera controller base

--- NutriFence (iOS)/NutriFence (iOS)/NFLabelCaptureViewController.swift
+++ NutriFence (iOS)/NutriFence (iOS)/NFLabelCaptureViewController.swift
@@ -11,16 +11,44 @@ import CameraManager
 
 class NFLabelCaptureViewController: UIViewController {
     
+    
+    @IBOutlet weak var cameraView: UIView!
     private let cameraManager = CameraManager()
-    
-    @IBOutlet weak var captureButton: UIButton!
-    
+    @IBOutlet weak var shutterButton: NFShutterButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        switch cameraManager.currentCameraStatus() {
+        case .notDetermined:
+            cameraManager.askUserForCameraPermission({ [unowned self] (isGranted) in
+                if isGranted {
+                    self.addCameraToView()
+                }
+            })
+        case .accessDenied:
+            cameraManager.showErrorBlock("Permission needed", "NutriFence needs access to the camera to function. Go to settings to enable camera access")
+        case .noDeviceFound: break
+        case .ready:
+            addCameraToView()
+        }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
     
+    @IBAction func shutterButtonTapped() {
+        
+    }
+    
+    fileprivate func addCameraToView() {
+        cameraManager.addPreviewLayerToView(cameraView, newCameraOutputMode: .stillImage)
+        cameraManager.showErrorBlock = { [weak self] (erTitle: String, erMessage: String) -> Void in
+            
+            let alertController = UIAlertController(title: erTitle, message: erMessage, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (alertAction) -> Void in  }))
+            
+            self?.present(alertController, animated: true, completion: nil)
+        }
+    }
 }

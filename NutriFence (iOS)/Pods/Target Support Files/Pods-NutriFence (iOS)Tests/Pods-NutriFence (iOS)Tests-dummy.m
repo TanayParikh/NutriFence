428a804c847b8b61af5de00b6de2f3f8c1d816bf
mgwatt0521 <mgwatt0521@gmail.com>
2016-12-20 02:13:12 -0600

    Fix podfile

---
+++ NutriFence (iOS)/Pods/Target Support Files/Pods-NutriFence (iOS)Tests/Pods-NutriFence (iOS)Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NutriFence__iOS_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_NutriFence__iOS_Tests
+@end
---
+++ NutriFence (iOS)/Pods/Target Support Files/Pods-NutriFence (iOS)UITests/Pods-NutriFence (iOS)UITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_NutriFence__iOS_UITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_NutriFence__iOS_UITestsVersionString[];
+
